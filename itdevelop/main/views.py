6ba4ddadeb6940483d3f5c5191421667bb6fece8
Daniyar Izteleuov <d.iztel@gmail.com>
2026-03-02 16:33:42 +0500

    commit 04-02 - Передача данных в шаблоны

--- itdevelop/main/views.py
+++ itdevelop/main/views.py
@@ -4,14 +4,17 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 
 # Create your views here.
+data = {
+    "title" : "Home Page",
+    "content" : "This is the home page",
+    "values": [100, 300, True],
+}
 def index(request):
-    return render(request, "main/index.html", {"title":"Home Page"})
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "main/index.html", data)
+
 
 def about(request):
     return render(request, "main/about.html", {"text":"About Us Page"})
-# def about(request):
-#     return HttpResponse("About is Reddy.")
+
 
 
